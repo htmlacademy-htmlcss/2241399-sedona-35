59e6c51470741de3bba9ac62b47a030a9406f806
Daria Sinko <daria_sinko@bk.ru>
2022-11-15 09:59:56 +0300

    fixes for linting

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const modal = document.querySelector('.modal-container');
 
 const buttonOpenModal = document.querySelector('.open-button');
 
-const butttonCloseModal = modal.querySelector('.modal-close-button');
-butttonCloseModal.addEventListener('click', () => {
-    modal.classList.add('visually-hidden');
+const buttonCloseModal = modal.querySelector('.modal-close-button');
+
+buttonCloseModal.addEventListener('click', () => {
+	modal.classList.add('visually-hidden');
 });
+
 buttonOpenModal.addEventListener('click', () => {
-    modal.classList.remove('visually-hidden');
+	modal.classList.remove('visually-hidden');
 });
+
 console.log(modal, buttonOpenModal, buttonCloseModal);
